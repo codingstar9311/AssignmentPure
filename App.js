dc084a776f39636168cfa545e90d2c0b1be650ca
jindev91 <jindevsky91@outlook.com>
2023-05-31 20:36:16 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = (props) => {
+  return (
+    <View>
+      <Text>Hello world</Text>
+    </View>
+  )
+}
+export default App;
